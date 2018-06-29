e202ba94357a1b80ba296c39903b3a4bf50d7cd9
tankeryang <youngzyang@outlook.com>
2018-06-29 12:09:12 +0800

    add: Properties, StringBuilder, TreeMap测试

--- src/com/yang/Main.java
+++ src/com/yang/Main.java
@@ -9,7 +9,6 @@ public class Main {
 
     public static Properties p = System.getProperties();
     public static void main(String[] args) {
-	// write your code here
         System.out.println(MyDate.countDiffDay("2018-01-03", "2018-01-02"));
         System.out.println(MyString.dumpProperties(p));
     }
